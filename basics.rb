c76fc4def3428cf3fc5fb8f7493146ae80da67c1
Sylvia <sylviakieha@gmail.com>
2016-03-18 22:05:45 +0300

    accessing url parameters

--- basics.rb
+++ basics.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 
+# basic routes
 get '/' do
   "Hello, World!"
 end
 
 get '/about' do
   "A little about me."
+end
+
+# accessing URL parameters
+get '/hello/:name/:city' do
+  "Hello there #{params[:name].swapcase}  from #{params[:city]}."
+end
+
+get '/more/*' do
+  params[:splat]
 end
